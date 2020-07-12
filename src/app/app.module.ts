b257531a4c2a9af395f10c403bea1f3898a8ffc2
luddoro <luddenr1@live.se>
2020-07-13 00:57:21 +0200

    WIP welcome page + sidebar
    Co-authored-by: Edbergarn <Edbergarn@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
-import { RecipeListPageComponent } from './components/recipe-list-page/recipe-list-page.component';
 import { ColumnsComponent } from './components/welcome-page/columns/columns.component';
+import { RecipeListPageComponent } from './components/recipe-list-page/recipe-list-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+
+/*Lägg Angular Material här */
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
     RecipeListPageComponent,
-    ColumnsComponent
+    ColumnsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
